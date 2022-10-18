728c8ebab0ad4dc0e19146cc714b7dbd57c79521
FabiAyacucho <fitoaya22@gmail.com>
2022-10-17 22:31:30 -0300

    cargando cambios

--- main.js
+++ main.js
@@ -1,1 +1,29 @@
 
+
+
+const resumen = document.querySelector("#resumen")
+const span = document.querySelector("#span")
+const select = document.getElementById('selector')
+const cantidad = document.getElementById('cantidad')
+const borrar = document.getElementById('borrar')
+let contador = 0
+
+ select.addEventListener('change', 
+ function(){
+ const valorDelSelect = this.options[select.selectedIndex]
+ //alert(select.value);
+ });
+
+ resumen.addEventListener('click',() => {
+ let sel = select.value;
+ let cant = cantidad.value;
+ contador  = cant * sel ;
+ //alert(contador)
+ span.textContent = contador
+ 
+    });
+
+ borrar.addEventListener('click',() =>{
+   contador = 0
+   span.textContent = contador
+    })   
